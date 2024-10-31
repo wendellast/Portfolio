ede6ad4519019fb189f027a01b600eb14cf6ffef
wendellast <wendelalves01a@hotmail.com>
2024-10-31 12:38:17 -0300

    display-music

--- app/assets/javascripts/music.js
+++ app/assets/javascripts/music.js
@@ -1,35 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var playButton = document.getElementById('playButton');
-    var audioPlayer = document.getElementById('audioPlayer');
-    var screenSize = window.innerWidth;
-  
-  
-    if (screenSize > 600) {
-    
-      playButton.style.display = 'none';
-  
-    }
-  
-    
-    var newAudioPlayer = document.getElementById('audioPlayer');
-    playButton.addEventListener('click', function () {
-      if (newAudioPlayer && newAudioPlayer.paused) {
-        newAudioPlayer.play();
-        playButton.textContent = 'Pause';
-      } else {
-        newAudioPlayer.pause();
-        playButton.textContent = 'Play';
-      }
-    });
-  
-    if (newAudioPlayer) {
-      newAudioPlayer.addEventListener('ended', function () {
-        playButton.textContent = 'Play';
-      });
+const playButton = document.getElementById('playButton');
+const audio = document.getElementById('minhaMusica');
+
+// Função para atualizar a visibilidade do botão e do controle de áudio
+function updateVisibility() {
+    const isSmallScreen = window.innerWidth <= 600; // Ajuste o valor conforme necessário
+
+    if (isSmallScreen) {
+        playButton.style.display = 'block'; // Exibe o botão
+        audio.style.display = 'none'; // Oculta o controle de áudio
     } else {
-      audioPlayer.addEventListener('ended', function () {
-        playButton.textContent = 'Play';
-      });
+        playButton.style.display = 'none'; // Oculta o botão
+        audio.style.display = 'inline'; // Exibe o controle de áudio
     }
-  });
-  
+}
+
+// Chama a função na carga da página
+updateVisibility();
+
+// Adiciona um ouvinte de evento para o redimensionamento da janela
+window.addEventListener('resize', updateVisibility);
+
+// Adiciona um ouvinte de evento para o botão
+playButton.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        playButton.textContent = 'Pause';
+    } else {
+        audio.pause();
+        playButton.textContent = 'Play';
+    }
+});
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_10_30_171058) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "description"
