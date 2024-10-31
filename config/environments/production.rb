01b85b5e09dd7fe10a3013767bc68183cb92f61e
wendellast <wendelalves01a@hotmail.com>
2024-10-30 21:05:19 -0300

    test: host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,10 +2,18 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  #
+
+  # Other configurations...
+
+
+
+
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
-  config.hosts << "wendellast.up.railway.app"
+  Rails.application.config.hosts << "wendellast.up.railway.app"
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
