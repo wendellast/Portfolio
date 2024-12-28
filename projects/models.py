c458f29a009f75e14dc369fb4a826a148dc80e86
wendellast <wendelalves01a@hotmail.com>
2024-12-28 11:23:08 -0300

    update -ok

--- projects/models.py
+++ projects/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='project_images/', blank=True, null=True)  # Permite nulo e opcional
+    image = models.ImageField(upload_to='project_images/', blank=True, null=True)
     tags = models.CharField(max_length=255, help_text="Separe as tags por vírgulas")
     website_link = models.URLField(blank=True, null=True)
     github_link = models.URLField(blank=True, null=True)
--- projects/views.py
+++ projects/views.py
@@ -4,6 +4,5 @@ from .models import Project
 def project_list(request):
     projects = Project.objects.all()
     for project in projects:
-        # Divide as tags em uma lista e remove espaços extras de cada tag
         project.tag_list = [tag.strip() for tag in project.tags.split(',')]
     return render(request, 'portfolio.html', {'projects': projects})
