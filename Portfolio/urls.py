8d81902001eee090bc4382c4e2391c5bf6e27a7c
wendellast <wendelalves01a@hotmail.com>
2024-12-28 16:30:24 -0300

    Portfolio --ok2

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
